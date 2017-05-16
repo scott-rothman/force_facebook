7430d2c59962cd3a6fb0c60a0a5d10ed2d827d5e
Scott Rothman <scottr@rga.com>
2017-05-16 17:03:12 -0400

    Add checks for existing query strings Add query strings to Home URLS that show on page

--- content.js
+++ content.js
@@ -12,3 +12,13 @@ if (on_facebook && !has_redirect && !has_query_string) {
     var new_url = url + '&sk=h_chr';
     window.location.replace(new_url);
 }
+
+window.setTimeout(function() {
+    console.log('Updating FB URLS');
+    //Update home buttons to have URL query on the end
+    //These classes are based on facebooks current markup.  If they change their naming convetions this will need to be updated
+    //Facebook icon
+    document.querySelector('._19eb').href = document.querySelector('._19eb').href + '&sk=h_chr';
+    //Home button
+    document.querySelector('#u_0_5').href = document.querySelector('#u_0_5').href + '&sk=h_chr';    
+}, 500);
